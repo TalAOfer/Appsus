921a4a30081d833d680f96dbd8cf504f531572fa
Eran Avichzer <eranavichzer@gmail.com>
2022-04-27 12:25:12 +0300

    First commit to emailApp

---
+++ apps/mail/pages/mail-display.jsx
@@ -0,0 +1,20 @@
+export class MailDisplay extends React.Component {
+    state = {
+
+    }
+
+    componentDidMount() {
+
+    }
+    render() {
+        return <section className="mail-display">
+            MailDisplay
+{/* 
+            <BookFilter filterBy={filterBy} onSetFilter={this.onSetFilter} />
+
+            <BookList selectBook={this.onSelectBook} books={books} /> */}
+
+        </section>
+    }
+
+}
---
+++ apps/mail/pages/mail-sidebar.jsx
@@ -0,0 +1,28 @@
+export class MailSideBar extends React.Component {
+    state = {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return <section className="mail-sidebar">
+            <button>+ Compose</button>
+            <ul>
+                <li>Inbok</li>
+                <li>Starred</li>
+                <li>Sent Mail</li>
+                <li>Drafts</li>
+            </ul>
+
+            <div>Email Read: 33%</div>
+
+
+
+
+        </section>
+    }
+
+}

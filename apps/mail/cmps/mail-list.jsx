dc00a6256d4f8cd9954cb6940abbf0f6c268d71e
Eran Avichzer <eranavichzer@gmail.com>
2022-04-28 15:49:43 +0300

    fix import7

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -1,42 +1,42 @@
-// import { EmailsPreview } from "../cmps/mail-preview.jsx";
-// import { EmailDetails } from "../pages/email-details.jsx";
-// import { EmailCompose } from "../pages/email-compose.jsx";
+import { EmailsPreview } from "../cmps/mail-preview.jsx";
+import { EmailDetails } from "../pages/email-details.jsx";
+import { EmailCompose } from "../pages/email-compose.jsx";
 // import { eventBusService } from "../../../services/event-bus-service.js";
 
-// // import { EmailFilter } from "../../mail/cmps/mail-filter.jsx";
-// // import { EmailFilter } from "../cmps/mail-filter.jsx";
+// import { EmailFilter } from "../../mail/cmps/mail-filter.jsx";
+// import { EmailFilter } from "../cmps/mail-filter.jsx";
 
-// import { EmailFilter } from "./../cmps/mail-filter.jsx";
+import { EmailFilter } from "./../cmps/mail-filter.jsx";
 
 const { Route, NavLink, Link } = ReactRouterDOM;
 
 export class EmailList extends React.Component {
 
-    // state = {
-    //     emailId: ''
-    // }
+    state = {
+        emailId: ''
+    }
 
-    // componentDidMount() {
-    //     this.removeEvent = eventBusService.on('email-id', (emailId) => {
-    //         this.setState({ emailId })
-    //     })
-    // }
+    componentDidMount() {
+        // this.removeEvent = eventBusService.on('email-id', (emailId) => {
+        //     this.setState({ emailId })
+        // })
+    }
 
-    // componentWillUnmount() {
-    //     this.removeEvent()
-    // }
+    componentWillUnmount() {
+        this.removeEvent()
+    }
 
     render() {
-        // const { emails, isReadUpdate, removeEmail, isStarUpdate, searchTxt } = this.props
-        // const { emailId } = this.state
+        const { emails, isReadUpdate, removeEmail, isStarUpdate, searchTxt } = this.props
+        const { emailId } = this.state
         return (
             <React.Fragment>
-                {/* <section className="email-list">
+                <section className="email-list">
                     <EmailFilter searchTxt={searchTxt}/>
                     {emailId && <Route path={`/email/${emailId}`} component={EmailDetails} />}
                     {<Route path={`/email/new_email`} component={EmailCompose} />}
                     {emails.map((email) => (<EmailsPreview key={email.id} email={email} isReadUpdate={isReadUpdate} isStarUpdate={isStarUpdate} removeEmail={removeEmail} />))}
-                </section> */}
+                </section>
             </React.Fragment>
         )
     }

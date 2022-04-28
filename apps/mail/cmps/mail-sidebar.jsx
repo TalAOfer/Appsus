2e06a0f7feb0da3a568464786966a1eb3dc704a7
Eran Avichzer <eranavichzer@gmail.com>
2022-04-28 17:48:13 +0300

    fix img path

--- apps/mail/cmps/mail-sidebar.jsx
+++ apps/mail/cmps/mail-sidebar.jsx
@@ -1,5 +1,6 @@
 import { eventBusService } from "../../../services/event-bus-service.js";
 
+
 const { NavLink } = ReactRouterDOM
 
 export class MailSideBar extends React.Component {
@@ -31,7 +32,7 @@ export class MailSideBar extends React.Component {
                 <NavLink to={`/email/new_email`}><button className="sidbar-btn">+ Compose</button></NavLink>
             </div>
             <ul className="sidbar-list">
-                <NavLink to="/email/inbox" ><li onClick={() => { status('inbox') }} className="filter-btn"><img src="../assets/img/mail/inbox.png"></img> &nbsp;&nbsp;&nbsp;&nbsp; Inbox </li></NavLink>
+                <NavLink to="/email/inbox" ><li onClick={() => { status('inbox') }} className="filter-btn"><img src="../../../assets/img/mail/inbox.png"></img> &nbsp;&nbsp;&nbsp;&nbsp; Inbox </li></NavLink>
                 <NavLink to="/email/starred" ><li onClick={() => { status('starred') }} className="filter-btn"> <img src="../assets/img/mail/star.png"></img> &nbsp;&nbsp;&nbsp;&nbsp; Starred </li></NavLink>
                 <NavLink to="/email/sent" ><li onClick={() => { status('sent') }} className="filter-btn"> <img src="../assets/img/mail/sent.png"></img> &nbsp;&nbsp;&nbsp;&nbsp; Sent Mail </li></NavLink>
                 <NavLink to="/email/Drafts" ><li onClick={() => { status('Drafts') }} className="filter-btn"> <img src="../assets/img/mail/draft.png"></img> &nbsp;&nbsp;&nbsp;&nbsp; Drafts </li></NavLink>

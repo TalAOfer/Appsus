071b1bc1b243eaef4f07526bbc09ac2ec77e4fd4
Eran Avichzer <eranavichzer@gmail.com>
2022-04-28 16:02:14 +0300

    fix import12

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -1,6 +1,6 @@
 import { EmailsPreview } from "../cmps/mail-preview.jsx";
-import { EmailDetails } from "../pages/email-details.jsx";
-import { EmailCompose } from "../pages/email-compose.jsx";
+// import { EmailDetails } from "../pages/email-details.jsx";
+// import { EmailCompose } from "../pages/email-compose.jsx";
 
 import { eventBusService } from "../../../services/event-bus-service.js";
 
@@ -34,8 +34,8 @@ export class EmailList extends React.Component {
             <React.Fragment>
                 <section className="email-list">
                     <EmailFilter searchTxt={searchTxt}/>
-                    {emailId && <Route path={`/email/${emailId}`} component={EmailDetails} />}
-                    {<Route path={`/email/new_email`} component={EmailCompose} />}
+                    {/* {emailId && <Route path={`/email/${emailId}`} component={EmailDetails} />}
+                    {<Route path={`/email/new_email`} component={EmailCompose} />} */}
                     {emails.map((email) => (<EmailsPreview key={email.id} email={email} isReadUpdate={isReadUpdate} isStarUpdate={isStarUpdate} removeEmail={removeEmail} />))}
                 </section>
             </React.Fragment>
